0ef7413087b57a4fc8b1d05a3154d970d1b3c898
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2021-04-19 11:43:19 +0900

    add: staff_branch.php

---
+++ staff_branch.php
@@ -0,0 +1,9 @@
+<?php
+
+if (isset($_POST['edit']) == true) {
+  $staff_code = $_POST['staffcode'];
+  header('Location: staff_edit.php?staffcode=' . $staff_code);
+} elseif (isset($_POST['delete']) == true) {
+  $staff_code = $_POST['staffcode'];
+  header('Location: staff_delete.php?staffcode=' . $staff_code);
+}
